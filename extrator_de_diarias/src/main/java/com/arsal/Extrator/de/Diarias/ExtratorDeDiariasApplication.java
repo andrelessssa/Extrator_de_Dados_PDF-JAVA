928fec9650c64e490a2f7b63ae1f5874439ad854
André Luiz Lessa <49814751+andrelessssa@users.noreply.github.com>
2025-11-05 23:11:16 -0300

    :tada:init Projeto iniciado
    :tada:init Projeto iniciado

---
+++ extrator_de_diarias/src/main/java/com/arsal/Extrator/de/Diarias/ExtratorDeDiariasApplication.java
@@ -0,0 +1,13 @@
+package com.arsal.Extrator.de.Diarias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExtratorDeDiariasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExtratorDeDiariasApplication.class, args);
+	}
+
+}
---
+++ extrator_de_diarias/src/test/java/com/arsal/Extrator/de/Diarias/ExtratorDeDiariasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arsal.Extrator.de.Diarias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExtratorDeDiariasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
